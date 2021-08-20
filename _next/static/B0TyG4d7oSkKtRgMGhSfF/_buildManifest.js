fbb22295f27fe76a3d6e47c257b786f983c484e7
LorikAliu <57730370+LorikAliu@users.noreply.github.com>
2021-08-20 11:30:03 +0000

    Deploying to gh-pages from @ LorikAliu/myportfolio@ad90c863c719e15bbca74f50d10ac755cd2f1c5f 🚀

--- _next/static/B0TyG4d7oSkKtRgMGhSfF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8319e747e547f71062be.css","static/chunks/pages/index-f0ea686e2bd3ae6c5072.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/MDL_2sMmq4DfUlI3FZR_h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d7477645197e37c96ff5.css","static/chunks/pages/index-41b3b99b3d7c5c628923.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
