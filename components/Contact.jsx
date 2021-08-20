ad90c863c719e15bbca74f50d10ac755cd2f1c5f
Lorik Aliu <lorikaliu77@gmail.com>
2021-08-20 13:29:09 +0200

    Styling-ContactForm-2

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,39 +3,11 @@ import contactStyles from '../styles/Contact.module.scss'
 
 const Contact = () => {
     return (
-        // <div className={contactStyles.contact} id="contact">
-        //     <div className={contactStyles.contactContainer}>
-        //         <div className={contactStyles.contactRowTop}>
-        //             <h1>Contact Me</h1>
-        //         </div>
-        //         <div className={contactStyles.contactRowBottom}>
-        //             <form action="https://formspree.io/f/mrgranee" method="POST" className={contactStyles.frm}>
-        //                 <div className={contactStyles.inpLabel}>
-        //                     <label htmlFor="name">Name</label>
-        //                     <input type="text" id="name" name="name" placeholder="Your name.."  required /> 
-        //                 </div>
-        //                 <div className={contactStyles.inpLabel}>
-        //                     <label htmlFor="email">Email</label>
-        //                     <input type="email" id="email" name="_replyto" placeholder="Your email.." required />
-        //                 </div>
-        //                 <div className={contactStyles.inpLabel}>
-        //                 <label htmlFor="subject">Comments/Questions</label>
-        //                     <textarea id="subject" name="message" placeholder="Your message.." style={{height: '200px'}}
-        //                     maxLength="1500" required>
-        //                     </textarea>
-        //                 </div>
-        //                 <div className={contactStyles.btnContainer}>
-        //                     <button type="submit" className={contactStyles.contactButton}>Submit</button>
-        //                 </div>
-        //             </form>
-        //         </div>
-        //     </div>
-        // </div>
         <div className={contactStyles.formContainer} id="contact">
             <form action="https://formspree.io/f/mrgranee" method="POST" >
                 <div className={contactStyles.formTitle}>
                     <div className={contactStyles.title}><h1>Get in touch</h1></div>
-                    <div className={contactStyles.subtitle}><h2>Tell me about your project or drop me a line. I'd love to hear from you</h2></div> 
+                    <div className={contactStyles.subtitle}><h2>Tell me about your project or drop me a line. I&apos;d love to hear from you</h2></div> 
                 </div>
                 <div className={contactStyles.inputsContainer}>
                     <div className={contactStyles.inputContainer}>
