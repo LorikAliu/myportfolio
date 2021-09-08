fa1d298a9af1e8927ef64537d9ebdd77b55a86c2
LorikAliu <57730370+LorikAliu@users.noreply.github.com>
2021-09-08 22:02:25 +0000

    Deploying to gh-pages from @ LorikAliu/myportfolio@70bf51367322a09a79fcf89b27eed35ce9a53c32 🚀

---
+++ _next/static/MOweMbc870QpuxZZ3et1C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/720-94b5a8e44d41346eab7f.js","static/css/f6489a5485ea9627955a.css","static/chunks/pages/index-673aaa21afccc58888dd.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
